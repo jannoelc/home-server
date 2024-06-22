5e29021c05d6769c4f09354fd65607e6c3a5519e
jannoelc <jannoel_c@proton.me>
2024-06-22 20:54:46 +0800

    add vpn install script

--- lxc/arr-stack/install.sh
+++ lxc/arr-stack/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DIR_NAME="jellyfin"
+DIR_NAME="arr-stack"
 
 cd ~
 if [ ! -d "$DIR_NAME" ]; then
@@ -9,15 +9,20 @@ if [ ! -d "$DIR_NAME" ]; then
 fi
 cd "$DIR_NAME"
 
-if [ ! -d config ]; then
-    echo "Creating config directory"
-    mkdir config
-fi
-
 echo "Updating docker-compose.yml file"
 wget https://raw.githubusercontent.com/jannoelc/home-server/develop/lxc/$DIR_NAME/docker-compose.yml -O docker-compose.yml
 
 if ! [ -e .env ]; then
     echo "Creating .env file"
     wget https://raw.githubusercontent.com/jannoelc/home-server/develop/lxc/$DIR_NAME/.env.sample -O .env
+fi
+
+if [ ! -d vpn ]; then
+    echo "Creating vpn directory"
+    mkdir vpn
+fi
+cd vpn
+if ! [ -e "wg0.conf" ]; then
+    echo "Creating wg0.conf file"
+    wget https://raw.githubusercontent.com/jannoelc/home-server/develop/lxc/$DIR_NAME/vpn/wg0.conf -O wg0.conf
 fi
