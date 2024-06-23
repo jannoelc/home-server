e2db599cc991e25a76ba88a8043bb3046dd99b08
jannoelc <jannoel_c@proton.me>
2024-06-23 18:49:02 +0800

    test new config for gluetun wireguard

--- lxc/arr-stack/install.sh
+++ lxc/arr-stack/install.sh
@@ -16,13 +16,3 @@ if ! [ -e .env ]; then
     echo "Creating .env file"
     wget https://raw.githubusercontent.com/jannoelc/home-server/develop/lxc/$DIR_NAME/.env.sample -O .env
 fi
-
-if [ ! -d vpn ]; then
-    echo "Creating vpn directory"
-    mkdir vpn
-fi
-cd vpn
-if ! [ -e "wg0.conf" ]; then
-    echo "Creating wg0.conf file"
-    wget https://raw.githubusercontent.com/jannoelc/home-server/develop/lxc/$DIR_NAME/vpn/wg0.conf -O wg0.conf
-fi
