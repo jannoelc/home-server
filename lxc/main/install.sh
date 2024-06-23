de957af431c17f6205f8333e55354d43ca74eef3
jannoelc <jannoel_c@proton.me>
2024-06-24 02:57:17 +0800

    experimenting with dnsmasq

--- lxc/main/install.sh
+++ lxc/main/install.sh
@@ -12,6 +12,13 @@ cd "$DIR_NAME"
 echo "Updating docker-compose.yml file"
 wget https://raw.githubusercontent.com/jannoelc/home-server/develop/lxc/$DIR_NAME/docker-compose.yml -O docker-compose.yml
 
+if ! [ -d tailscale_dns ]; then
+    echo "Creating tailscale_dns directory"
+fi
+
+wget https://raw.githubusercontent.com/jannoelc/home-server/develop/lxc/$DIR_NAME/tailscale_dns/dnsmasq.conf -O tailscale_dns/dnsmasq.conf
+wget https://raw.githubusercontent.com/jannoelc/home-server/develop/lxc/$DIR_NAME/tailscale_dns/resolv.conf -O tailscale_dns/dnsmasq.conf
+
 if ! [ -e .env ]; then
     echo "Creating .env file"
     wget https://raw.githubusercontent.com/jannoelc/home-server/develop/lxc/$DIR_NAME/.env.sample -O .env
