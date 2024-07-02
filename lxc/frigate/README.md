3cb335c53845fd2e635369de34f1ceb39f56808b
jannoelc <jannoel.calayag@gmail.com>
2024-07-03 03:55:56 +0800

    add initial frigate setup script

---
+++ lxc/frigate/README.md
@@ -0,0 +1,15 @@
+# Frigate
+
+## Setup
+
+To install or update, run:
+
+```bash
+bash -c "$(wget -qO - https://raw.githubusercontent.com/jannoelc/home-server/develop/lxc/frigate/install.sh)"
+```
+
+Start using the command below:
+
+```bash
+docker compose up -d
+```
---
+++ lxc/frigate/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DIR_NAME="frigate"
+
+cd ~
+if [ ! -d "$DIR_NAME" ]; then
+    echo "Creating $DIR_NAME directory"
+    mkdir "$DIR_NAME"
+fi
+cd "$DIR_NAME"
+
+echo "Updating docker-compose.yml file"
+wget https://raw.githubusercontent.com/jannoelc/home-server/develop/lxc/$DIR_NAME/docker-compose.yml -O docker-compose.yml
+
